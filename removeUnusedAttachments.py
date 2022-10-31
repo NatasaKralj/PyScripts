30551fa285e67b1f32d06968bdd04c0edd94a87c
Johanna Hemminger <johanna.hemminger@mendix.com>
2022-10-31 16:59:47 +0100

    Finished script!

--- removeUnusedAttachments.py
+++ removeUnusedAttachments.py
@@ -6,9 +6,50 @@ import os
 import json
 import csv
 import logging
+import re
 
 # Grab working directory
 startDir = input('Specify FULL PATH to local content directory: ')
 
 # Empty lists to help with parsing data
-attachmentList = []
+attachmentList = [] 
+
+# Walk through all files in directory
+for dirPath, dirNames, allFiles in os.walk(startDir):
+    for name in allFiles:
+        # File path that includes name of file
+        filePath = os.path.join(dirPath, name)
+        # Relative file path that includes name of file
+        relFilePath = os.path.relpath(filePath, startDir)
+        # Pattern to find any /attachments/ followed by either ) or "
+        fullAttachmentRefSearch = '((/attachments/([-./\+\w\+ ]*)?)(\)|"))'
+        # Checks if file is Markdown
+        if name.lower().endswith('.md'):
+            # Opens Markdown file
+            with open(filePath, mode='r', encoding="utf-8") as file:
+                for line in file:
+                    fullSearch = re.search(fullAttachmentRefSearch,line)
+                    if fullSearch != None:
+                        # Creates a dictionary for file with aliases
+                        itemDict = {"File path": relFilePath, "File name": name, "Attachment Link": fullSearch.group(2)}
+                        # Appends each dictionary to attachmentList
+                        attachmentList.append(itemDict)
+
+# Walk through all attachments and check that they're in the attachmentList
+# Grab attachment directory
+attachmentDir = input('Specify FULL PATH to attachments directory (for example, C:\\Users\\Johanna.Hemminger\\docs\\static\\attachments): ')
+for dirPath, dirNames, allFiles in os.walk(attachmentDir):
+    for name in allFiles:
+        # File path that includes name of file
+        filePath = os.path.join(dirPath, name)
+        # Relative file path that includes name of file
+        relFilePath = os.path.relpath(filePath, attachmentDir)
+        newFilePath = relFilePath.replace(os.sep, '/')
+        deleteFlag = True
+        for item in attachmentList:
+            inList = item["Attachment Link"].endswith(newFilePath)
+            if inList is True:
+                deleteFlag = False
+                break
+        if deleteFlag is True:
+            os.remove(filePath)
