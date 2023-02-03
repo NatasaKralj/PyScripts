a6e18672fc5da8ee5bad0cc921d506267c65fd50
Johanna Hemminger <johanna.hemminger@mendix.com>
2023-02-03 14:42:45 +0100

    Fixed regex

--- removeUnusedAttachments.py
+++ removeUnusedAttachments.py
@@ -4,12 +4,17 @@ removeUnusedAttachments.py goes through all .md files to make a list of attachme
 
 import os
 import re
+import logging
 
 # Grab working directory
-startDir = input('Specify FULL PATH to local content directory: ')
+startDir = "C:\\Users\\Johanna.Hemminger\\docs\\content"
+#startDir = input('Specify FULL PATH to local content directory: ')
 
 # Empty lists to help with parsing data
 attachmentList = [] 
+
+# Create log file
+logging.basicConfig(filename='attachmentlistlog.log', filemode='w', format='%(message)s')
 
 # Walk through all files in directory
 for dirPath, dirNames, allFiles in os.walk(startDir):
@@ -19,7 +24,7 @@ for dirPath, dirNames, allFiles in os.walk(startDir):
         # Relative file path that includes name of file
         relFilePath = os.path.relpath(filePath, startDir)
         # Pattern to find any /attachments/ followed by either ) or "
-        fullAttachmentRefSearch = '/attachments/([-./\+\w\+ ]*)?(?:\)|)"'
+        fullAttachmentRefSearch = '/attachments/([-./\+\w= ]*)?(?:\)|")'
         # Checks if file is Markdown
         if name.lower().endswith('.md'):
             # Opens Markdown file
@@ -27,14 +32,17 @@ for dirPath, dirNames, allFiles in os.walk(startDir):
                 for line in file:
                     fullSearch = re.findall(fullAttachmentRefSearch,line)
                     if fullSearch != []:
-                        # Creates a dictionary for file with aliases
-                        itemDict = {"File path": relFilePath, "File name": name, "Attachment Link": fullSearch}
-                        # Appends each dictionary to attachmentList
-                        attachmentList.append(itemDict)
+                        for searchitem in fullSearch:
+                            # Creates a dictionary for file with aliases
+                            itemDict = {"File path": relFilePath, "File name": name, "Attachment Link": searchitem}
+                            # Appends each dictionary to attachmentList
+                            attachmentList.append(itemDict)
+logging.warning(attachmentList)
 
 # Walk through all attachments and check that they're in the attachmentList
 # Grab attachment directory
-attachmentDir = input('Specify FULL PATH to attachments directory (for example, C:\\Users\\Johanna.Hemminger\\docs\\static\\attachments): ')
+attachmentDir = "C:\\Users\\Johanna.Hemminger\\docs\\static\\attachments"
+# attachmentDir = input('Specify FULL PATH to attachments directory (for example, C:\\Users\\Johanna.Hemminger\\docs\\static\\attachments): ')
 for dirPath, dirNames, allFiles in os.walk(attachmentDir):
     for name in allFiles:
         # File path that includes name of file
@@ -45,8 +53,8 @@ for dirPath, dirNames, allFiles in os.walk(attachmentDir):
         deleteFlag = True
         for item in attachmentList:
             # fullSearch returns a list, so we need to pull things out of it
-            inList = item["Attachment Link"].endswith(newFilePath)
-            if inList is True:
+            inList = item["Attachment Link"]
+            if name in inList:
                 deleteFlag = False
                 break
         if deleteFlag is True:
